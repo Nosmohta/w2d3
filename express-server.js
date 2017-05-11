058f909b467d31554b3cdc55799fb2edf35bd7b5
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-11 01:08:17 +0000

    Stable. Cookies added on username POST from header.

--- express-server.js
+++ express-server.js
@@ -4,9 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
 
-
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,7 +14,15 @@ const urlDatabase = {
   "sdfjkb": "http://www.wikipedia.com"
 };
 
+
 function startServer() {
+
+  let templateVars = {
+    urls: urlDatabase,
+    username: "",
+    curShortURL: ""
+  }
+
   app.set("view engine", "ejs");
 
   app.use(bodyParser.urlencoded({extended: true}));
@@ -31,41 +38,57 @@ function startServer() {
     res.render("urls_index", templateVars);
   });
 
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  })
-
-  app.post("/urls/", (req, res) => {
+  app.post("/urls", (req, res) => {
     let shortURL = generateRandomString(urlDatabase);
     urlDatabase[shortURL] = req.body.longURL;
-    //res.statusCode = 302
     res.redirect("/urls/"+shortURL)
   });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new", templateVars);
+  })
 
   app.post("/urls/:id/delete", (req, res) => {
       console.log("I recieved a post req to delete")
       console.log(req.params.id);
       delete urlDatabase[req.params.id];
       res.redirect("/urls");
-  })
+  });
 
   app.get("/u/:shortURL", (req, res) => {
     let longURL = urlDatabase[req.params.shortURL]
     res.redirect(longURL);
   });
 
-
   app.get("/urls/:id", (req, res) => {
-    res.render("urls_show", {shortURL: req.params.id,
-                             urls: urlDatabase}
-    );
+    templateVars.curShortURL = req.params.id;
+    res.render("urls_show", templateVars);
   });
 
-  app.post(("/urls/:id"), (req, res) => {
+  app.post( "/urls/:id", (req, res) => {
     urlDatabase[req.params.id] = req.body.updateURL;
     res.redirect("/urls/");
   });
 
+
+  app.get( "/login", (req, res) => {
+    res.send( "You have tried to login");
+  });
+
+  app.post( "/login", (req, res) => {
+    //needs to check for preexisting usernames
+    console.log(req.body);
+    res.cookie( "username", req.body.username)
+    res.redirect("/login/" + req.body.username);
+  });
+
+
+
+  app.get(("/login/:username"), (req, res) => {
+    console.log("This person has logged in:" + req.params.username);
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 
   app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
